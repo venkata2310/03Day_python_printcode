561ce51a01c60adc5a3425c6d74cced003ddbf99
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-13 19:41:33 +0530

    Add files via upload

---
+++ Print_helloworld.py
@@ -0,0 +1,75 @@
+#print('python programming')
+#print("python program")
+
+"""error code for line 5 and 6"""
+#print('python programm'ing')
+#print("python"program")
+
+#print('python progra"ming')
+#print("pyton pro'gram")
+
+#print('python program\'ming')
+#print("python prog\"am")
+
+#print('we are learning \ python programming')
+
+#print("we are learning \
+#Python Programming")
+
+#print('we are learning \
+#python programming')
+
+#print(''' python programming ''')
+
+#print(""" we are learning python programming """)
+
+#print('''we are learn'ing pyth"on programming''')
+#print(""" we are learn'ing pyth"on programming""")
+
+#print('''I'm doing python programming''')
+#print('''I\'m doing python programming''')
+
+#print('hello world')
+#print()
+#print("ok i take care")
+
+#print(type(''))
+#print(type(""))
+#print(type(''''''))
+#print(type(""""""""))
+
+#print('hello \
+  #  \
+    #python')
+
+#print(''' hello
+
+#python''')
+
+""" To print hello world in all possible ways """
+#print('hello world')
+#print("hello world")
+#print('''hello world''')
+#print("""hello world""")
+
+"""to print hello wor'ld in all possible ways"""
+#print("hello wor\'ld")
+#print("hello wor'ld")
+#print('''hello wor'ld''')
+#print("""Hello Wor'ld""")
+
+"""To print  print(Hello World) in all possible ways """
+#print('print(hello world)')
+#print("print(Hello World)")
+#print('''print(Hello World)''')
+#print("""print(Hello World)""")
+
+"""  A String can be multiplied only with integer (int) """
+
+#print('hello ' * 5)
+#print(  3 *  'hello ')
+
+""" A string cannot be multiplied with str and float """
+
+print('hey' * 'bye')
+print('hey' * 3.5)
