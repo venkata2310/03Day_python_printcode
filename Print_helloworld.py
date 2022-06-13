b702e9d3f2dd1bae67abede5db440ef59c3fe09d
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-13 10:46:41 +0530

    Add files via upload

---
+++ Print_helloworld.py
@@ -0,0 +1,27 @@
+#print('python programming')
+#print("python program")
+
+"""error code for line 5 and 6"""
+#print('python programm'ing')
+#print("python"program")
+
+#print('python progra"ming')
+#print("pyton pro'gram")
+
+#print('python program\'ming')
+#print("python prog\"am")
+
+#print('we are learning \ python programming')
+
+#print("we are learning \
+#Python Programming")
+
+#print('we are learning \
+#python programming')
+
+#print(''' python programming ''')
+
+#print(""" we are learning python programming """)
+
+
+
